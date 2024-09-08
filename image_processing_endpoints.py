d5425e59204967eef4837ed0c2a5c0a600b9d69f
Canip Paçacı <91578178+canippacaci@users.noreply.github.com>
2024-09-08 22:21:13 +0300

    Update image_processing_endpoints.py

--- image_processing_endpoints.py
+++ image_processing_endpoints.py
@@ -1,26 +1,18 @@
 from fastapi import APIRouter, HTTPException, UploadFile, File
 from image_processing_service import process_image_file  # Import the service function
+import io
 
 router = APIRouter()
-
 
 @router.post("/process/")
 async def process_image_endpoint(file: UploadFile = File(...)):
     """
     Endpoint to process an uploaded image using Otsu's thresholding and filling closed areas.
-
-    Parameters:
-    - file: The image file to be processed.
-
-    Returns:
-    - dict: Processed image results including Otsu's thresholding and filled result.
     """
     try:
         # Read the uploaded image
         contents = await file.read()
-        temp_image_path = "temp_image.png"
-        with open(temp_image_path, "wb") as f:
-            f.write(contents)
+        temp_image_path = io.BytesIO(contents)
 
         # Process the image
         otsu_result, filled_otsu = process_image_file(temp_image_path)
